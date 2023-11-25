7ed683314b2603e9841715ccf9bc7b451f56f12c
ixxr <904188433@qq.com>
2023-11-25 15:49:44 +0800

    第三次

--- app/src/main/java/com/jnu/student/MainActivity.java
+++ app/src/main/java/com/jnu/student/MainActivity.java
@@ -1,11 +1,15 @@
 package com.jnu.student;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
+    private TextView textView1;
+    private TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +20,35 @@ public class MainActivity extends AppCompatActivity {
         //textView.setText(R.id.text_vciew_hellow_world);
         // 将TextView设置为Activity的内容视图
         // 通过资源名称获取字符串资源的ID
-        int resId = getResources().getIdentifier("hello_android", "string", getPackageName());
+
         // 通过资源ID获取字符串值
-        String helloAndroid = getResources().getString(resId);
+
         // 找到TextView
-        TextView textView = findViewById(R.id.test_view_hello_world);
+
         // 设置文本内容
-        textView.setText(helloAndroid);
+
         //setContentView(textView);
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(v -> {
+            String tempText = textView1.getText().toString();
+            textView1.setText(textView2.getText());
+            textView2.setText(tempText);
+
+            showToast();
+            showAlertDialog();
+        });
+    }
+    private void showToast() {
+        Toast.makeText(this, "交换成功", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("交换成功")
+                .setPositiveButton("OK", null)
+                .show();
     }
 }
